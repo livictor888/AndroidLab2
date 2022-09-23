80beb1b3f3a34ad9f772944346e4b732a6aae945
livictor888 <https://github.com/livictor888/COMP3761>
2022-09-23 02:17:07 -0700

    lab2

--- app/src/main/java/com/example/androidlab2/MainActivity.java
+++ app/src/main/java/com/example/androidlab2/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.androidlab2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +20,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void changeBackground(View view) {
+        Random random = new Random();
+        int color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
+        ConstraintLayout testView = findViewById(R.id.background);
+        testView.setBackgroundColor(color);
 
     }
 
-    public void textToSpeech() {
-
+    public void textToSpeech(View view) {
+        Intent speechIntent = new Intent(this, SpeakActivity.class);
+        startActivity(speechIntent);
     }
 
     public void showAPIVersion() {
-
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        int version = Build.VERSION.SDK_INT;
+        String versionRelease = Build.VERSION.RELEASE;
+        String messageText = " manufacturer " + manufacturer
+                + " \n model " + model
+                + " \n version " + version
+                + " \n versionRelease " + versionRelease;
     }
 
     public void showSerialNumber() {
---
+++ app/src/main/java/com/example/androidlab2/SpeakActivity.java
@@ -0,0 +1,10 @@
+package com.example.androidlab2;
+
+import android.speech.tts.TextToSpeech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SpeakActivity extends AppCompatActivity {
+    TextToSpeech tts;
+
+}
