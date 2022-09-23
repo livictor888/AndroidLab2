cfa1a97a62ba0e6fcf6b9d117a4b0a9ff00dae63
livictor888 <https://github.com/livictor888/COMP3761>
2022-09-23 11:36:34 -0700

    lab2

--- app/src/main/java/com/example/androidlab2/MainActivity.java
+++ app/src/main/java/com/example/androidlab2/MainActivity.java
@@ -7,7 +7,11 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -24,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
         int color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
         ConstraintLayout testView = findViewById(R.id.background);
         testView.setBackgroundColor(color);
-
     }
 
     public void textToSpeech(View view) {
@@ -32,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(speechIntent);
     }
 
-    public void showAPIVersion() {
+    public void showAPIVersion(View view) {
         String manufacturer = Build.MANUFACTURER;
         String model = Build.MODEL;
         int version = Build.VERSION.SDK_INT;
@@ -41,9 +44,22 @@ public class MainActivity extends AppCompatActivity {
                 + " \n model " + model
                 + " \n version " + version
                 + " \n versionRelease " + versionRelease;
+        Log.e("MainActivity", messageText);
+        Toast toast = Toast.makeText(getApplicationContext(), messageText,Toast.LENGTH_SHORT);
+        TextView toastView =  toast.getView().findViewById(android.R.id.message);
+        toastView.setTextColor(Color.RED);
+        toast.show() ;
     }
 
-    public void showSerialNumber() {
-
+    public void showSerialNumber(View view) {
+        String deviceId = Settings.System.getString(getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+        Log.e("MainActivity", deviceId);
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, deviceId);
+        sendIntent.setType("text/plain");
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
     }
 }
--- app/src/main/java/com/example/androidlab2/SpeakActivity.java
+++ app/src/main/java/com/example/androidlab2/SpeakActivity.java
@@ -1,10 +1,48 @@
 package com.example.androidlab2;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.Locale;
+import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Locale;
+
 public class SpeakActivity extends AppCompatActivity {
     TextToSpeech tts;
+    EditText text;
+    Button btn;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech);
+
+        text = findViewById(R.id.textInputEditText);
+        btn = findViewById(R.id.speakButton);
+
+        tts = new TextToSpeech(getApplicationContext(), status -> {
+            if (status != TextToSpeech.ERROR) {
+                tts.setLanguage(Locale.UK);
+            }
+        });
+    }
+
+    public void speakButton(View view) {
+        tts.speak(text.getText().toString(), TextToSpeech.QUEUE_FLUSH,null, null);
+    }
+
+    public void backButton(View view) {
+        Intent speechIntent = new Intent(this, MainActivity.class);
+        startActivity(speechIntent);
+    }
 }
+
+
