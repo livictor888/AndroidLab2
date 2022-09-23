179f228b72988f0a15d544e3fc0e86a51f6b5197
livictor888 <https://github.com/livictor888/COMP3761>
2022-09-23 01:27:30 -0700

    init

---
+++ app/src/main/java/com/example/androidlab2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.androidlab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeBackground(View view) {
+
+    }
+
+    public void textToSpeech() {
+
+    }
+
+    public void showAPIVersion() {
+
+    }
+
+    public void showSerialNumber() {
+
+    }
+}
